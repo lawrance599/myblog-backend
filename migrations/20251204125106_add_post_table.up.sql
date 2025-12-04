9d220f520fcb8e7956e9f6fd19b6ac86c7c4e3d1
yixin <fanyixin050909@outlook.com>
2025-12-04 22:03:50 +0800

    feat(migrate): 添加了博文数据存储的升级和降级脚本 feat(servie): 完善了service以及postservice的相关测试

--- migrations/20251204125106_add_post_table.up.sql
+++ migrations/20251204125106_add_post_table.up.sql
@@ -1,8 +1,9 @@
--- Add migration script here
+-- Add up migration script here
 CREATE TABLE post  (
     id SERIAL PRIMARY KEY,
     tags JSONB NOT NULL,
+    title VARCHAR(255) NOT NULL,
     first_publish TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     last_modify TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    query_count INTEGER NOT NULL DEFAULT 0
+    count INTEGER NOT NULL DEFAULT 0
 );
--- src/service/post.rs
+++ src/service/post.rs
@@ -1,38 +1,46 @@
 use sqlx::{FromRow, Pool};
 use sqlx::types::chrono::{DateTime, Utc};
 use sqlx::types::Json;
-#[derive(FromRow)]
+use tokio::task::JoinSet;
+#[allow(dead_code)]
+#[derive(FromRow, Debug)]
 pub struct Post{
     id: i32,
     title: String,
     tags: Json<Vec<String>>,
     first_publish: DateTime<Utc>,
     last_modify: DateTime<Utc>,
-    query_count: i32,
+    count: i32,
 }
+
 
 /// PostService
 /// 博文有关的数据库操作的封装
-pub struct PostService<'a>(&'a Pool<sqlx::Postgres>);
-impl PostService<'_> {
-    pub fn new(pool:&'_  Pool<sqlx::Postgres>) -> PostService<'_> {
+pub struct PostService(Pool<sqlx::Postgres>);
+impl Clone for PostService {
+    fn clone(&self) -> Self {
+        PostService(self.0.clone())
+    }
+}
+impl PostService {
+    pub fn new(pool:  Pool<sqlx::Postgres>) -> PostService {
         PostService(pool)
     }
     pub async fn get_post_by_id(&self, id: i32) -> Result<Post, sqlx::Error> {
-        let _ = sqlx::query_as::<_, Post>("SELECT * FROM posts WHERE id = $1")
+        let _ = sqlx::query_as::<_, Post>("SELECT * FROM post WHERE id = $1")
             .bind(id)
-            .fetch_one(&self.0.clone())
+            .fetch_one(&self.0)
             .await?;
 
-        self.increase_query_count(id).await
+        self.increase_count(id).await
     }
     pub async fn get_all_posts(&self) -> Result<Vec<Post>, sqlx::Error> {
-        let posts = sqlx::query_as::<_, Post>("SELECT * FROM posts")
-            .fetch_all(&self.0.clone())
+        let posts = sqlx::query_as::<_, Post>("SELECT * FROM post")
+            .fetch_all(&self.0)
             .await?;
         let mut last_error: Option<sqlx::Error> = None;
         for post in posts.iter() {
-            let _ = match self.increase_query_count(post.id).await {
+            let _ = match self.increase_count(post.id).await {
                 Ok(_) => {}
                 Err(e) => {
                     last_error = Some(e);
@@ -46,26 +54,110 @@ impl PostService<'_> {
         }
     }
     pub async fn create_post(&self, title: String, tags: Vec<String>) -> Result<Post, sqlx::Error> {
-        sqlx::query_as::<_, Post>("INSERT INTO posts (title, tags) VALUES ($1, $2) RETURNING *")
+        sqlx::query_as::<_, Post>("INSERT INTO post (title, tags) VALUES ($1, $2) RETURNING *")
             .bind(title)
             .bind(Json(tags))
-            .fetch_one(&self.0.clone())
+            .fetch_one(&self.0)
             .await
     }
 
     pub async fn update_post(&self, id: i32, title: String, tags: Vec<String>) -> Result<Post, sqlx::Error> {
-        sqlx::query_as::<_, Post>("UPDATE posts SET title = $1, tags = $2 WHERE id = $3 RETURNING *")
+        sqlx::query_as::<_, Post>("UPDATE post SET title = $1, tags = $2 WHERE id = $3 RETURNING *")
             .bind(title)
             .bind(Json(tags))
             .bind(id)
-            .fetch_one(&self.0.clone())
+            .fetch_one(&self.0)
             .await
     }
 
-    async fn increase_query_count(&self, id: i32) -> Result<Post, sqlx::Error> {
-        sqlx::query_as::<_, Post>("UPDATE posts SET query_count = query_count + 1 WHERE id =  RETURNING *")
+    async fn increase_count(&self, id: i32) -> Result<Post, sqlx::Error> {
+        sqlx::query_as::<_, Post>("UPDATE post SET count = count + 1 WHERE id = $1 RETURNING *")
             .bind(id)
-            .fetch_one(&self.0.clone())
+            .fetch_one(&self.0)
             .await
     }
+
+    pub async fn delete_post(&self, id: i32) -> Result<Post, sqlx::Error> {
+        sqlx::query_as::<_, Post>("DELETE FROM post WHERE id = $1 RETURNING *")
+            .bind(id)
+            .fetch_one(&self.0)
+            .await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fake::{Dummy, Fake, Faker};
+
+    #[derive(Dummy)]
+    struct FakePostInsert {
+        title: String,
+        tags: Vec<String>,
+    }
+
+    /// 生成一个假的 Post 实例
+    fn generate_fake_post() -> FakePostInsert {
+        let fake_post: FakePostInsert = Faker.fake();
+        fake_post.into()
+    }
+
+    /// 生成多个假的 Post 实例
+    fn generate_fake_posts(count: usize) -> Vec<FakePostInsert> {
+        (0..count).map(|_| generate_fake_post()).collect()
+    }
+
+
+    #[tokio::test]
+    async fn test_generate_fake_post() {
+        let post = generate_fake_post();
+        
+        // 验证生成的数据
+        assert!(!post.title.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_generate_fake_posts() {
+        let posts = generate_fake_posts(5);
+        assert_eq!(posts.len(), 5);
+        
+        for post in posts {
+            assert!(!post.title.is_empty());
+        }
+    }
+
+    async fn generate_post_service() -> PostService{
+        let pool = sqlx::PgPool::connect("postgres://yixin:yixin@localhost:5432/test").await.unwrap();
+        PostService::new(pool)
+    }
+
+    #[tokio::test]
+    async fn test_insert_and_get_post() {
+        let service = generate_post_service().await;
+        let fake_post = generate_fake_post();
+        let inserted_post = service.create_post(fake_post.title.clone(), fake_post.tags.clone()).await;
+        assert!(inserted_post.is_ok());
+        let post = service.get_post_by_id(inserted_post.unwrap().id).await;
+        assert!(post.is_ok());
+        let post = post.unwrap();
+        assert_eq!(post.title.clone(), fake_post.title);
+        assert_eq!(post.tags.as_ref(), &fake_post.tags);
+        assert!(service.delete_post(post.id).await.is_ok())
+    }
+
+    #[tokio::test]
+    async fn test_query_count_works_ok(){
+        let service = generate_post_service().await;
+        let fake_post = generate_fake_post();
+        let inserted_post = service.create_post(fake_post.title.clone(), fake_post.tags.clone()).await;
+        assert!(inserted_post.is_ok());
+        let inserted_post = inserted_post.unwrap();
+        assert_eq!(inserted_post.count, 0);
+        let again_post = service.get_post_by_id(inserted_post.id).await;
+        assert!(again_post.is_ok());
+        let again_post = again_post.unwrap();
+        assert_eq!(again_post.count, 1);
+        assert!(service.delete_post(again_post.id).await.is_ok())
+    }
+
 }
